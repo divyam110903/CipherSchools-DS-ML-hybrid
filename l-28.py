6413718b206104f97051733f2f01cfaea4b087a5
divyam110903 <117055743+divyam110903@users.noreply.github.com>
2024-06-30 23:46:52 +0530

    Update l-28.py

--- l-28.py
+++ l-28.py
@@ -98,18 +98,68 @@ print("After Creating Polynomial Features:\n", df_poly)
 #1. VARIANCE THRESHOLDING
 #Variance thresholding is a feature selection method that removes features with low variance.
 
+import pandas as pd
+from sklearn.feature_selection import VarianceThreshold
+data={
+    'Feature1': [1,1,1,1,1],
+    'Feature2': [2, 3, 4, 5, 6],
+    'Feature3': [10, 20, 30, 40, 50],
+    'Feature4': [1,1,1,1,1]
+}
+df=pd.DataFrame(data)
 
-
-
+selector=VarianceThreshold(threshold=0.1)
+df_variance_filtered =pd.DataFrame(selector.fit_transform(df), columns=df.columns[selector.get_support()])
+print("After Variance Thresholding:\n", df_variance_filtered)
 
 
 
 #2.CORRELATION MATRIX
 #Correlation matrix is a matrix that shows the correlation between features. Removing correlated data to reduce reduntancy
 
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#data
+data={
+    'Feature1': [1,2,3,4,5],
+    'Feature2': [1, 4, 9, 16, 25],
+    'Feature3': [1, 8, 27, 64, 125],
+    'Feature4': [1, 9,2,6,10]
+}
+
+df=pd.DataFrame(data)
+
+corr_matrix=df.corr().abs()
+
+plt.figure(figsize=(10,8))
+sns.heatmap(corr_matrix, annot=True)
+plt.show()
+#upper triangle remove kr rhe
+upper= corr_matrix.where(np.triu(np.ones(corr_matrix),k=1).astype(bool))
 
 
+to_drop= [column for column in upper.columns if any(upper[column]>0.9)]
 
+
+df_new= df.drop(to_drop,axis=1)
+print("After filtering")
+print(df_new)
 
 #3 Domain knowledge
 #Domain knowledge is the knowledge of the domain of the problem. 
+
+data={
+    'Age':[25,30,30,40,55],
+    'Salary':[50000,56000,60000,90000,555900],
+    'Height':[5.5,6.0,5.8,5.9,6.1],
+    'Weight':[60,70,65,75,80]
+
+}
+df =pd.DataFrame(data)
+
+selected_features_domain=df[['Age','Salary']]
+print("Selected Features on Domain Knowledge: \n", selected_features_domain)
+
