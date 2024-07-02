2b70f7bab627a8b0af84dcd03bf107bdaf7dabd7
divyam110903 <117055743+divyam110903@users.noreply.github.com>
2024-07-02 15:44:28 +0530

    rm lec-30 -> Linear Regression.py

--- lec-30 -> Linear Regression.py
+++ lec-30 -> Linear Regression.py
@@ -1,5 +1,5 @@
 # Linear Regression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split.
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
